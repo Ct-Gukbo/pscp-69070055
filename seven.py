0aa808ca191c563faed99f34b383b43c841faf2e
Ct-Gukbo <captainnattanadej@gmail.com>
2026-07-06 21:47:25 +0700

    finsih seven

--- seven.py
+++ seven.py
@@ -2,6 +2,11 @@
 def main():
     """seven"""
     sev = int(input())
-    x = 7 ** sev
-    print((str(x))[-1])
+
+    if not sev:
+        print(1)
+    else:
+        last_digit = [7, 9, 3, 1]
+        print(last_digit[(sev - 1) % 4])
+
 main()
