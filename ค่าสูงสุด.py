0c4f4fdf92cde2b2cafff641568b1067ad2b3953
Gukbo <captainnattanadej@gmail.com>
2026-07-17 16:03:17 +0700

    ค่าสูงสุด changed

--- ค่าสูงสุด.py
+++ ค่าสูงสุด.py
@@ -4,7 +4,18 @@ def main():
     a = int(input())
     b = int(input())
     c = int(input())
-    numlist = [a,b,c]
-    highest = max(numlist)
-    print(highest)
+    if a > b and a > c:
+        print(a)
+    elif b > a and b > c:
+        print(b)
+    elif c > a and c > b:
+        print(c)
+    elif a == b > c:
+        print(a,b)
+    elif b == c > a:
+        print(b,c)
+    elif c == a > b:
+        print(a,c)
+    else :
+        print(a,b,c)
 main()
